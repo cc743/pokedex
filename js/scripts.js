bf7e9999cbb57b154384fcb8e8ec3ff7340f4b81
cc743 <67940367+cc743@users.noreply.github.com>
2020-09-16 18:29:25 -0400

    create 3 pokemon objects --> develop array of said objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,19 @@
-alert('Hello World');
+let pokemon1 = {
+  name: 'Bulbasaur',
+  height: 0.7,  //note: height is in m(meters)
+  types: ['grass', 'poison']
+};
 
-let favoriteFood = 'Steak';
-document.write(favoriteFood);
+let pokemon2 = {
+  name: 'Charizard',
+  height: 1.7,
+  types: ['fire', 'flying']
+};
+
+let pokemon3 = {
+  name: 'Psyduck',
+  height: 0.8,
+  types: ['water']
+}
+
+let pokemonList = [pokemon1, pokemon2, pokemon3];
