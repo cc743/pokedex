4b067a416fb54345ddb270816fa4d46055d1ecdd
cc743 <67940367+cc743@users.noreply.github.com>
2020-09-17 18:38:51 -0400

    modification of the definition array pokemonList
    In this case, each pokemon object is defined within the array itself

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,33 @@
-let bulbasaur = {
+//modified pokemonList to be an array with  objects created inside said array
+let pokemonList = [
+{
   name: 'Bulbasaur',
   height: 0.7,  //note: height is in m(meters)
   types: ['grass', 'poison']
-};
+},
 
-let charizard = {
+{
   name: 'Charizard',
   height: 1.7,
   types: ['fire', 'flying']
-};
+},
 
-let psyduck = {
+{
   name: 'Psyduck',
   height: 0.8,
   types: ['water']
-}
+},
 
-let pidgeot = {
+{
   name: 'Pidgeot',
   height: 1.5,
   types: ['flying', 'normal']
 }
+];
 
-let pokemonList = [bulbasaur, charizard, psyduck, pidgeot];
+//let pokemonList = [bulbasaur, charizard, psyduck, pidgeot]; <--old code
+
+//defining strings for use in for loop
 let string1 = ' (height:'
 let string2 = ')'
 
@@ -31,5 +36,5 @@ for (let i = 0; i < pokemonList.length; i++){
   if (pokemonList[i].height > 0.9)
   document.write(" -- This pokemon is tall.")
 
-  document.write("<br />");
+  document.write("<br />");  //inclusion of a line break for the website
 }
